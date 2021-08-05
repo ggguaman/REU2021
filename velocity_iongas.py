68ae6bd829ad2a2c4b0f1d712f85128bdfce1e82
Giselle Guaman <gguaman@hamilton.internal.amnh.org>
2021-08-05 14:38:24 +0000

    updated

--- velocity_iongas.py
+++ velocity_iongas.py
@@ -11,9 +11,9 @@ s.physical_units()
 pynbody.analysis.angmom.sideon(s)
 
 #filter to only use ionised gas
-GasFilter = pynbody.filt.HighPass('temp','4.2')
+GasFilter = pynbody.filt.HighPass('temp','15848 K')
 
 #creating image to show velocity of ionised gas
-sph.image(s.g,GasFilter,qty="vr",width=10,denoise=True,approximate_fast=False,log=False)
+sph.image(s.g[GasFilter],qty="vr",vmin=-20,width=10,denoise=True,approximate_fast=False,log=False)
 
 plt.show()
