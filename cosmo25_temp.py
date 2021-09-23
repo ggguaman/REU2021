3f4d9d885aba1248fc6d19e040b0500bed85ca98
Giselle Guaman <gguaman@hamilton.internal.amnh.org>
2021-09-23 15:49:41 +0000

    cosmo25 code: gas temp with BH pos

--- cosmo25_temp.py
+++ cosmo25_temp.py
@@ -12,10 +12,32 @@ s=pynbody.load('/mnt/data0/jillian/gguaman/cosmo25p.768sg1bwK1BHe75.008192')
 s.physical_units()
 
 #centering halo
-pynbody.analysis.angmom.sideon(s)
+pynbody.analysis.angmom.faceon(s)
 
 #creating slice to show gas temp
-sph.image(s.g,qty="temp",width=10,denoise=True,approximate_fast=False,log=True)
+sph.image(s.g,qty="temp",width=30,denoise=True,approximate_fast=False,log=True)
 
-plt.show()
-#plt.savefig("cosmo25_tempslice.png")
+#function to find BH
+def findBH(s):
+    BHfilter=pynbody.filt.LowPass('tform',0.0)
+    BH=s.stars[BHfilter]
+    return BH
+BH=findBH(s)
+print("The number of black holes is",len(BH))
+
+#distance BH is from galaxy
+#with pynbody.analysis.halo.center(s, mode='hyb'):
+print([s],['pos'])
+
+BHposition = BH['pos']
+print("The black hole's postion is", BHposition)
+#print(BH['pos'].in_units('kpc'))
+
+for i in range(len(BH)):
+    BHx=BHposition[[i],0]
+    BHy=BHposition[[i],1]
+    BHz=BHposition[[i],2]
+    plt.plot(BHx,BHy, 'ro')
+
+#plt.show()
+plt.savefig("cosmo25_temp.png")
