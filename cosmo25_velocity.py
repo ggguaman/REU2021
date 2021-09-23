659eaf05f2b77e96b616b2e927716d70eeaf4980
Giselle Guaman <gguaman@hamilton.internal.amnh.org>
2021-09-23 16:07:40 +0000

    cosmo25 code: gas velocity with BH pos

--- cosmo25_velocity.py
+++ cosmo25_velocity.py
@@ -11,10 +11,31 @@ s=pynbody.load('/mnt/data0/jillian/gguaman/cosmo25p.768sg1bwK1BHe75.008192')
 s.physical_units()
 
 #centering halo
-pynbody.analysis.angmom.sideon(s)
+pynbody.analysis.angmom.faceon(s)
 
 #creating gas velocity plot
-sph.image(s.g,qty="vr",denoise=True,approximate_fast=False,log=False)
+sph.image(s.g,qty="vr",width=30,denoise=True,approximate_fast=False,log=False)
 
-plt.show()
-#plt.savefig("cosmo25_velocity.png")
+#function to find BH
+def findBH(s):
+    BHfilter = pynbody.filt.LowPass('tform',0.0)
+    BH = s.stars[BHfilter]
+    return BH
+BH = findBH(s)
+print("The number of black holes is",len(BH))
+
+#distance BH is from galaxy
+#with pynbody.analysis.halo.center(s, mode='hyb'):
+print([s],['pos'])
+
+BHposition = BH['pos']
+print("The black hole's position is", BHposition)
+
+for i in range(len(BH)):
+    BHx=BHposition[[i],0]
+    BHy=BHposition[[i],1]
+    BHz=BHposition[[i],2]
+    plt.plot(BHx,BHy, 'ro')
+
+#plt.show()
+plt.savefig("cosmo25_velocity.png")
