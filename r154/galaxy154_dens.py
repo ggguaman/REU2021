09792fbbec6b28f4fa897473ac6857b72fdc16d4
Giselle Guaman <gguaman@hamilton.internal.amnh.org>
2021-09-30 17:26:21 +0000

    galaxy154 code: gas dens with BHs pos

---
+++ r154/galaxy154_dens.py
@@ -0,0 +1,40 @@
+#galaxy 154 gas density code
+import pynbody
+import pynbody.plot.sph as sph
+import matplotlib.pyplot as plt
+
+#loading snapshot
+s=pynbody.load('/mnt/data0/jillian/gguaman/r154/cosmo25p.768sg1bwK1BHe75.007779')
+
+#setting to physical units
+s.physical_units()
+
+#centering halo and aligning disk
+pynbody.analysis.angmom.faceon(s)
+
+#creating gas density slice
+sph.image(s.g,qty="rho",units="g cm^-3",width=50)
+
+#function to find BH
+def findBH(s):
+    BHfilter = pynbody.filt.LowPass('tform',0.0)
+    BH = s.stars[BHfilter]
+    return BH
+BH = findBH(s)
+print("The number of black holes is",len(BH))
+
+#distance BH is from galaxy
+#with pynbody.analysis.halo.center(s, mode='hyb'):
+print([s],['pos'])
+
+BHposition = BH['pos']
+print("The black hole's position is", BHposition)
+
+for i in range(len(BH)):
+    BHx=BHposition[[i],0]
+    BHy=BHposition[[i],1]
+    BHz=BHposition[[i],2]
+    plt.plot(BHx,BHy, 'ro')
+
+#plt.show()
+plt.savefig("galaxy154_dens.png")
