03f604736d0dbe0d942d8c70585a872d4696152a
Giselle Guaman <gguaman@hamilton.internal.amnh.org>
2021-09-30 19:15:27 +0000

    galaxy109 code: ionized gas with BHs pos

---
+++ r109/galaxy109_iongas.py
@@ -0,0 +1,46 @@
+#galaxy 109 ionized gas code with BHs pos
+import pynbody
+import numpy as np
+import pandas as pd
+import pynbody.plot.sph as sph
+import matplotlib.pyplot as plt
+from pynbody import filt, array
+
+#loading snapshot
+s=pynbody.load('/mnt/data0/jillian/gguaman/cosmo25p.768sg1bwK1BHe75.008192')
+
+#setting to physical units
+s.physical_units()
+
+#centering halo
+pynbody.analysis.angmom.faceon(s)
+
+#filter to only use ionised gas
+GasFilter = pynbody.filt.HighPass('temp','15848 K')
+
+#creating image to show velocity of ionised gas
+sph.image(s.g[GasFilter],qty="vr",vmin=-20,width=30,denoise=True,approximate_fast=False,log=False)
+
+#function to find BH
+def findBH(s):
+    BHfilter = pynbody.filt.LowPass('tform',0.0)
+    BH = s.stars[BHfilter]
+    return BH
+BH = findBH(s)
+print("The number of black holes is",len(BH))
+
+#distance BH is from galaxy
+#with pynbody.analysis.halo.center(s, mode='hyb'):
+print([s],['pos'])
+
+BHposition = BH['pos']
+print("The black hole's position is", BHposition)
+
+for i in range(len(BH)):
+    BHx=BHposition[[i],0]
+    BHy=BHposition[[i],1]
+    BHz=BHposition[[i],2]
+    plt.plot(BHx,BHy, 'ro')
+
+#plt.show()
+plt.savefig("galaxy109_iongas.png")
