cc06ff9fdfc5f2781fca1b670261b247743868fb
Giselle Guaman <gguaman@hamilton.internal.amnh.org>
2021-10-07 19:50:25 +0000

    galaxy 310 code: gas density

---
+++ r310/galaxy310_dens.py
@@ -0,0 +1,19 @@
+#galaxy 310 gas density code
+import pynbody
+import pynbody.plot.sph as sph
+import matplotlib.pyplot as plt
+
+#loading snapshot
+s=pynbody.load('/mnt/data0/jillian/gguaman/r310/cosmo25p.768sg1bwK1BHe75.007779')
+
+#setting to physical units
+s.physical_units()
+
+#centering halo and aligning disk
+pynbody.analysis.angmom.faceon(s)
+
+#creating gas density slice
+sph.image(s.g,qty="rho",units="g cm^-3",width=50)
+
+#plt.show()
+plt.savefig("galaxy310_dens.png")
