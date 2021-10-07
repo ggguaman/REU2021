d5d0d6c4f3fea9e646cb2b6ab84a2bd8b4534c01
Giselle Guaman <gguaman@hamilton.internal.amnh.org>
2021-10-07 14:54:38 +0000

    galaxy 168 code: stars with BH pos

---
+++ r168/galaxy168_stars.py
@@ -0,0 +1,19 @@
+#galaxy 168 stars code
+import pynbody
+import pynbody.plot.sph as sph
+import matplotlib.pylab as plt
+
+#loading snapshot
+s=pynbody.load('/mnt/data0/jillian/gguaman/r168/cosmo25p.768sg1bwK1BHe75.007779')
+
+#setting to physical units
+s.physical_units()
+
+#centering halo
+pynbody.analysis.angmom.faceon(s)
+
+#creating image using default bands(i,v,u)
+pynbody.plot.stars.render(s,width='40 kpc')
+
+#plt.show()
+plt.savefig("galaxy168_stars.png")
