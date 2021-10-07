d41d9dabbffed797234cc8c1f6eb9f04b55baaf3
Giselle Guaman <gguaman@hamilton.internal.amnh.org>
2021-10-07 17:31:04 +0000

    galaxy 219 code: gas histogram

---
+++ r219/galaxy219_gashist.py
@@ -0,0 +1,24 @@
+#galaxy 219 gas histogram
+import pynbody
+import numpy as np
+from matplotlib import colors
+import matplotlib.pyplot as plt
+
+#loading snapshot
+s=pynbody.load('/mnt/data0/jillian/gguaman/r219/cosmo25p.768sg1bwK1BHe75.007779')
+
+#setting to physical units
+s.physical_units()
+
+#centering halo
+pynbody.analysis.angmom.faceon(s)
+
+plt.hist(np.log10(s.g['temp']),color='green',bins='fd')
+
+#customizing hist
+plt.xlabel("Log(temp)")
+plt.ylabel("Number of particles")
+plt.title('Gas temp')
+
+#plt.show()
+plt.savefig("galaxy219_gashist.png")
