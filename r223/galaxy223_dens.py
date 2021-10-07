c03f843df9dbddee62dcf75e21d33a6b89e7d267
Giselle Guaman <gguaman@hamilton.internal.amnh.org>
2021-10-07 17:55:45 +0000

    galaxy 223 code: gas density

---
+++ r223/galaxy223_dens.py
@@ -0,0 +1,19 @@
+#galaxy 223 gas density code with BH pos
+import pynbody
+import pynbody.plot.sph as sph
+import matplotlib.pyplot as plt
+
+#loading snapshot
+s=pynbody.load('/mnt/data0/jillian/gguaman/r223/cosmo25p.768sg1bwK1BHe75.007779')
+
+#setting to physical units
+s.physical_units()
+
+#centering halo and aligning disk
+pynbody.analysis.angmom.faceon(s)
+
+#creating gas density slice
+sph.image(s.g,qty="rho",units="g cm^-3",width=10)
+
+#plt.show()
+plt.savefig("galaxy223_dens.png")
