6ffca1cf66ced7368ae9070b2965d510cc3ce2f0
Giselle Guaman <gguaman@hamilton.internal.amnh.org>
2021-10-07 17:55:04 +0000

    galaxy 223 code: no black hole

---
+++ r223/galaxy223_BHpos.py
@@ -0,0 +1,20 @@
+#galaxy 223 NO BH 
+import pynbody
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from pynbody import filt, array
+
+#loading snapshot
+s=pynbody.load('/mnt/data0/jillian/gguaman/r223/cosmo25p.768sg1bwK1BHe75.007779')
+
+#setting physical units
+s.physical_units()
+
+#function to find BH
+def findBH(s):
+    BHfilter=pynbody.filt.LowPass('tform',0.0)
+    BH=s.stars[BHfilter]
+    return BH
+BH=findBH(s)
+print("The number of black holes is",len(BH))
