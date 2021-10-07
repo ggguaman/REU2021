0c70eb16dbd5cb229f3d1a29de4e1207e27ce42e
Giselle Guaman <gguaman@hamilton.internal.amnh.org>
2021-10-07 19:54:57 +0000

    galaxy 310 code: gas velocity

---
+++ r310/galaxy310_velocity.py
@@ -0,0 +1,21 @@
+#galaxy 310 gas velocity code
+import pynbody
+import numpy as np
+import pandas as pd
+import pynbody.plot.sph as sph
+import matplotlib.pyplot as plt
+
+#loading snapshot
+s=pynbody.load('/mnt/data0/jillian/gguaman/r310/cosmo25p.768sg1bwK1BHe75.007779')
+
+#setting to physical units
+s.physical_units()
+
+#centering halo
+pynbody.analysis.angmom.faceon(s)
+
+#creating gas velocity plot
+sph.image(s.g,qty="vr",width=50,denoise=True,approximate_fast=False,log=False)
+
+#plt.show()
+plt.savefig("galaxy310_velocity.png")
