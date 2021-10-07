91d457adb7006d8acb6397bc5381f511eb6d7276
Giselle Guaman <gguaman@hamilton.internal.amnh.org>
2021-10-07 15:38:11 +0000

    galaxy 142 code: stars

---
+++ r142/galaxy142_stars.py
@@ -0,0 +1,19 @@
+#galaxy 142 stars code
+import pynbody
+import pynbody.plot.sph as sph
+import matplotlib.pylab as plt
+
+#loading snapshot
+s=pynbody.load('/mnt/data0/jillian/gguaman/r142/cosmo25p.768sg1bwK1BHe75.007779')
+
+#setting to physical units
+s.physical_units()
+
+#centering halo
+pynbody.analysis.angmom.faceon(s)
+
+#creating image using default bands(i,v,u)
+pynbody.plot.stars.render(s,width='20 kpc')
+
+#plt.show()
+plt.savefig("galaxy142_stars.png")
