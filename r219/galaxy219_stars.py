1d05bf6b0fc76d163dc6365b3a0f0169a7957aa1
Giselle Guaman <gguaman@hamilton.internal.amnh.org>
2021-10-07 17:32:35 +0000

    galaxy 219 code: stars

---
+++ r219/galaxy219_stars.py
@@ -0,0 +1,19 @@
+#galaxy 219 stars code
+import pynbody
+import pynbody.plot.sph as sph
+import matplotlib.pylab as plt
+
+#loading snapshot
+s=pynbody.load('/mnt/data0/jillian/gguaman/r219/cosmo25p.768sg1bwK1BHe75.007779')
+
+#setting to physical units
+s.physical_units()
+
+#centering halo
+pynbody.analysis.angmom.faceon(s)
+
+#creating image using default bands(i,v,u)
+pynbody.plot.stars.render(s,width='20 kpc')
+
+#plt.show()
+plt.savefig("galaxy219_stars.png")
