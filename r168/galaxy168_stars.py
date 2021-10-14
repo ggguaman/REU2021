db55cc034eeabfe6604f943e847fc2a06e347d09
Giselle Guaman <gguaman@hamilton.internal.amnh.org>
2021-10-14 19:35:05 +0000

    galaxy 168 code: stars

--- r168/galaxy168_stars.py
+++ r168/galaxy168_stars.py
@@ -13,7 +13,7 @@ s.physical_units()
 pynbody.analysis.angmom.faceon(s)
 
 #creating image using default bands(i,v,u)
-pynbody.plot.stars.render(s,width='40 kpc')
+pynbody.plot.stars.render(s,width='18 kpc')
 
 #plt.show()
 plt.savefig("galaxy168_stars.png")
