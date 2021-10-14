8d6863c120dfc098ac7473f7e220f4b134716adf
Giselle Guaman <gguaman@hamilton.internal.amnh.org>
2021-10-14 14:55:22 +0000

    galaxy 330 code: ionized gas

---
+++ r330/galaxy330_iongas.py
@@ -0,0 +1,25 @@
+#galaxy 330 ionized gas code
+import pynbody
+import numpy as np
+import pandas as pd
+import pynbody.plot.sph	as sph
+import matplotlib.pyplot as plt
+from pynbody import filt, array
+
+#loading snapshot
+s=pynbody.load('/mnt/data0/jillian/gguaman/r330/cosmo25p.768sg1bwK1BHe75.007779')
+
+#setting to physical units
+s.physical_units()
+
+#centering halo
+pynbody.analysis.angmom.faceon(s)
+
+#filter to only use ionised gas
+GasFilter = pynbody.filt.HighPass('temp','15848 K')
+
+#creating image to show velocity of ionised gas
+sph.image(s.g[GasFilter],qty="vr",vmin=-20,width=40,denoise=True,approximate_fast=False,log=False)
+
+#plt.show()
+plt.savefig("galaxy330_iongas.png")
