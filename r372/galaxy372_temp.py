b30fb27115d3fd80bb723b053a6efa97c02ceb07
Giselle Guaman <gguaman@hamilton.internal.amnh.org>
2021-10-14 15:55:21 +0000

    galaxy 372 code: gas temperature

---
+++ r372/galaxy372_temp.py
@@ -0,0 +1,21 @@
+#galaxy 372 gas temp code
+import pynbody
+import numpy as np
+from matplotlib import colors
+import pynbody.plot.sph as sph
+import matplotlib.pyplot as plt
+
+#loading snapshot
+s=pynbody.load('/mnt/data0/jillian/gguaman/r372/cosmo25p.768sg1bwK1BHe75.007779')
+
+#setting to physical units
+s.physical_units()
+
+#centering halo
+pynbody.analysis.angmom.faceon(s)
+
+#creating slice to show gas temp
+sph.image(s.g,qty="temp",width=20,denoise=True,approximate_fast=False,log=True)
+
+#plt.show()
+plt.savefig("galaxy372_temp.png")
