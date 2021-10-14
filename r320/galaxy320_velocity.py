cce7c1ac5ec03be87bdb6c5a652fe6c1eb93ace8
Giselle Guaman <gguaman@hamilton.internal.amnh.org>
2021-10-14 14:21:28 +0000

    galaxy 320 code: gas velocity with BHs pos

---
+++ r320/galaxy320_velocity.py
@@ -0,0 +1,46 @@
+#galaxy 320 gas velocity code with BH pos
+import pynbody
+import numpy as np
+import pandas as pd
+import pynbody.plot.sph as sph
+import matplotlib.pyplot as plt
+
+#loading snapshot
+s=pynbody.load('/mnt/data0/jillian/gguaman/r320/cosmo25p.768sg1bwK1BHe75.007779')
+
+#setting to physical units
+s.physical_units()
+
+#centering halo
+pynbody.analysis.angmom.faceon(s)
+
+#creating gas velocity plot
+sph.image(s.g,qty="vr",width=30,denoise=True,approximate_fast=False,log=False)
+
+#function to find BH
+def findBH(s):
+    BHfilter=pynbody.filt.LowPass('tform',0.0)
+    BH=s.stars[BHfilter]
+    return BH
+BH=findBH(s)
+print("The number of black holes is",len(BH))
+
+#distance BH is from galaxy
+#with pynbody.analysis.halo.center(s, mode='hyb'):
+print([s],['pos'])
+
+BHposition = BH['pos']
+print("The black hole's postion is", BHposition)
+print(BH['pos'].in_units('kpc'))
+
+for i in range(len(BH)):
+    #putting BH x in column
+    BHx=BHposition[[i],0]
+    #putting BH y in column
+    BHy=BHposition[[i],1]
+    #putting BH z in column
+    BHz=BHposition[[i],2]
+    plt.plot(BHx,BHy, 'ro')
+
+#plt.show()
+plt.savefig("galaxy320_velocity.png")
