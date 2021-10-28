cf14919e93d3eec3ed7a63cae2eff7e06e5bb41e
Giselle Guaman <gguaman@hamilton.internal.amnh.org>
2021-10-28 16:33:46 +0000

    axis updated

--- r372/galaxy372_dens.py
+++ r372/galaxy372_dens.py
@@ -13,7 +13,7 @@ s.physical_units()
 pynbody.analysis.angmom.faceon(s)
 
 #creating gas density slice
-sph.image(s.g,qty="rho",units="g cm^-3",width=20)
+sph.image(s.g,qty="rho",units="g cm^-3",width=40)
 
 #plt.show()
 plt.savefig("galaxy372_dens.png")
