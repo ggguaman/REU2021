86727ca08b0e4fac9642d12f9ca0568c28b14772
Giselle Guaman <gguaman@hamilton.internal.amnh.org>
2021-11-04 20:20:00 +0000

    side on added

--- r330/galaxy330_temp.py
+++ r330/galaxy330_temp.py
@@ -12,10 +12,12 @@ s=pynbody.load('/mnt/data0/jillian/gguaman/r330/cosmo25p.768sg1bwK1BHe75.007779'
 s.physical_units()
 
 #centering halo
-pynbody.analysis.angmom.faceon(s)
+#pynbody.analysis.angmom.faceon(s)
+pynbody.analysis.angmom.sideon(s)
 
 #creating slice to show gas temp
 sph.image(s.g,qty="temp",width=40,denoise=True,approximate_fast=False,log=True)
 
 #plt.show()
-plt.savefig("galaxy330_temp.png")
+#plt.savefig("galaxy330_temp.png")
+plt.savefig("galaxy330_temp(side).png")
