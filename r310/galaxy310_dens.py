50c892c248b906a0cbc5919d71d810c8c9077184
Giselle Guaman <gguaman@hamilton.internal.amnh.org>
2021-11-04 14:45:48 +0000

    axis updated

--- r310/galaxy310_dens.py
+++ r310/galaxy310_dens.py
@@ -10,10 +10,12 @@ s=pynbody.load('/mnt/data0/jillian/gguaman/r310/cosmo25p.768sg1bwK1BHe75.007779'
 s.physical_units()
 
 #centering halo and aligning disk
-pynbody.analysis.angmom.faceon(s)
+#pynbody.analysis.angmom.faceon(s)
+pynbody.analysis.angmom.sideon(s)
 
 #creating gas density slice
 sph.image(s.g,qty="rho",units="g cm^-3",width=40)
 
 #plt.show()
-plt.savefig("galaxy310_dens.png")
+#plt.savefig("galaxy310_dens.png")
+plt.savefig("galaxy310_dens(side).png")
