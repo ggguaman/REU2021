b9b57563873e621f92063a7bc2c16b49cfad1704
Giselle Guaman <gguaman@hamilton.internal.amnh.org>
2021-11-04 15:48:49 +0000

    axis updated

--- r142/galaxy142_dens.py
+++ r142/galaxy142_dens.py
@@ -10,8 +10,8 @@ s=pynbody.load('/mnt/data0/jillian/gguaman/r142/cosmo25p.768sg1bwK1BHe75.007779'
 s.physical_units()
 
 #centering halo and aligning disk
-#pynbody.analysis.angmom.faceon(s)
-pynbody.analysis.angmom.sideon(s)
+pynbody.analysis.angmom.faceon(s)
+#pynbody.analysis.angmom.sideon(s)
 
 #creating gas density slice
 sph.image(s.g,qty="rho",units="g cm^-3",width=20)
@@ -48,4 +48,4 @@ plt.plot(BHx,BHy, 'ro')
 
 #plt.show()
 plt.savefig("galaxy142_dens.png")
-plt.savefig("galaxy142_dens(side).png")
+#plt.savefig("galaxy142_dens(side).png")
