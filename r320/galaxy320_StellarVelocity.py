9b77a6ae213c8413d65f577186c7aa4c7822cf98
Giselle Guaman <gguaman@hamilton.internal.amnh.org>
2022-02-17 18:41:16 +0000

    Ray and GG StelVel code

--- r320/galaxy320_StellarVelocity.py
+++ r320/galaxy320_StellarVelocity.py
@@ -1,51 +1,17 @@
-#galaxy 320 stellar velocity code with BH pos
+#galaxy 320 Ray's stellar velocity maps
 import pynbody
 import numpy as np
-import pandas as pd
 import pynbody.plot.sph as sph
 import matplotlib.pyplot as plt
+from pynbody_velmaps.scripts.plot_manga_velmaps import *
 
-#loading snapshot
-s=pynbody.load('/mnt/data0/jillian/gguaman/r320/cosmo25p.768sg1bwK1BHe75.007779')
+filename='/mnt/data0/jillian/gguaman/r320/cosmo25p.768sg1bwK1BHe75.007779'
+redshift=0.05
+image_width=20
 
-#setting to physical units
-s.physical_units()
+stellar_map, ax = plot_star_map(filename,redshift,image_width,orientation="sideon")
+#stellar_pa = calc_pa(stellar_map)
 
-#centering halo
-#pynbody.analysis.angmom.faceon(s)
-pynbody.analysis.angmom.sideon(s)
-
-#creating star velocity plot
-sph.image(s.s,qty="vx",av_z="mass",width='20 kpc',cmap="PuOr", log=False, units='km s**-1')
-
-#function to find BH
-def findBH(s):
-    BHfilter=pynbody.filt.LowPass('tform',0.0)
-    BH=s.stars[BHfilter]
-    return BH
-BH=findBH(s)
-print("The number of black holes is:",len(BH))
-
-BHposition = BH['pos']
-#print("The black hole's postion is", BHposition)
-print("The BHs position is",BH['pos'].in_units('kpc'))
-
-for i in range(len(BH)):
-    #putting BH x in column
-    BHx=BHposition[[i],0]
-    #putting BH y in column
-    BHy=BHposition[[i],1]
-    #putting BH z in column
-    BHz=BHposition[[i],2]
-    plt.plot(BHx,BHy, 'ro')
-    #sphere filter
-    #radius = "1.25 kpc"
-    #center = (BHx[0], BHy[0], BHz[0])
-    #sphere = s.g[pynbody.filt.Sphere(radius, center)]
-    #print("The number of particles in the sphere is:",len(sphere))
-    #print("The stellar velocity is",sphere["vel"])
-    #print("The average stellar velocity is:",sphere["vel"].mean())
-
-#plt.show()
-#plt.savefig("galaxy320_StelVel.png")
-plt.savefig("galaxy320_StelVel(side).png")
+plt.show()
+#plt.savefig("galaxy320_StelVelMap.png")
+#plt.savefig("galaxy320_StelVelMap(side).png")
