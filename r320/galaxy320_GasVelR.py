b6b07c31a3dcbd72fcf8f8f5d9296c8cfd08c26f
Giselle Guaman <gguaman@hamilton.internal.amnh.org>
2022-02-24 17:27:19 +0000

    Ray gas velocity code

--- r320/galaxy320_GasVelR.py
+++ r320/galaxy320_GasVelR.py
@@ -7,14 +7,14 @@ from pynbody_velmaps.position_angles import *
 from pynbody_velmaps.scripts.plot_manga_velmaps import *
 
 filename='/mnt/data0/jillian/gguaman/r320/cosmo25p.768sg1bwK1BHe75.007779'
-redshift=0.05
+redshift=0.03
 image_width=20
 
-gas_map, ax = plot_gas_map(filename, redshift, image_width, orientation="sideon")
+gas_map, ax = plot_manga_map(filename, redshift, "gas", image_width, vmin = -20, vmax = 40, orientation="sideon")
 #gas_pa = calc_pa(gas_map)
-plt.colorbar(ax.images[0])
-plt.xlabel('x/kpc')
-plt.ylabel('y/kpc')
+#plt.colorbar(ax.images[0])
+#plt.xlabel('x/kpc')
+#plt.ylabel('y/kpc')
 
-#plt.show()
-plt.savefig("galaxy320_GasVelR.png")
+plt.show()
+#plt.savefig("galaxy320_GasVelR.png")
