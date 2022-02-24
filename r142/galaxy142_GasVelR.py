e2b2af9a840e6fe6a35f222f084e117fad6e8192
Giselle Guaman <gguaman@hamilton.internal.amnh.org>
2022-02-24 16:06:54 +0000

    Ray gas velocity code

---
+++ r142/galaxy142_GasVelR.py
@@ -0,0 +1,20 @@
+#galaxy 142 Ray's gas velocity code
+import pynbody
+import numpy as np
+import pynbody.plot.sph as sph
+import matplotlib.pyplot as plt
+from pynbody_velmaps.position_angles import *
+from pynbody_velmaps.scripts.plot_manga_velmaps import *
+
+filename='/mnt/data0/jillian/gguaman/r142/cosmo25p.768sg1bwK1BHe75.007779'
+redshift=0.05
+image_width=20
+
+gas_map, ax = plot_gas_map(filename, redshift, image_width, orientation="sideon")
+#gas_pa = calc_pa(gas_map)
+plt.colorbar(ax.images[0])
+plt.xlabel('x/kpc')
+plt.ylabel('y/kpc')
+
+#plt.show()
+plt.savefig("galaxy142_GasVelR.png")
