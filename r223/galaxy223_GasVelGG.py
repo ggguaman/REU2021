1958ecf39a9e2be5c27b486a9ef4eeb902e1ca97
Giselle Guaman <gguaman@hamilton.internal.amnh.org>
2022-03-03 19:54:28 +0000

    gas & stellar velocity

---
+++ r223/galaxy223_GasVelGG.py
@@ -0,0 +1,29 @@
+#galaxy 223 GG Gas velocity code
+import pynbody
+import numpy as np
+import pynbody.plot.sph as sph
+import matplotlib.pyplot as plt
+
+#loading snapshot
+s=pynbody.load('/mnt/data0/jillian/gguaman/r223/cosmo25p.768sg1bwK1BHe75.007779')
+
+#setting to physical units
+s.physical_units()
+
+#centering halo
+#pynbody.analysis.angmom.faceon(s)
+pynbody.analysis.angmom.sideon(s)
+
+#creating star velocity plot
+sph.image(s.g,qty="vz",width='20 kpc',cmap="RdBu", log=False, units='km s**-1')
+
+#function to find BH
+def findBH(s):
+    BHfilter=pynbody.filt.LowPass('tform',0.0)
+    BH=s.stars[BHfilter]
+    return BH
+BH=findBH(s)
+print("The number of black holes is:",len(BH))
+    
+plt.show()
+plt.savefig("galaxy223_GasVelGG.png")
