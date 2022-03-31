fffbce1f07e4ddf1a497457a7f78fc9fe7f3f031
Giselle Guaman <gguaman@hamilton.internal.amnh.org>
2022-03-31 19:43:29 +0000

    centered galaxy using stars to match Ray's code

--- r239/galaxy239_StelVelGG.py
+++ r239/galaxy239_StelVelGG.py
@@ -4,6 +4,63 @@ import numpy as np
 import pynbody.plot.sph as sph
 import matplotlib.pyplot as plt
 
+def stars_sideon(
+    h,
+    vec_to_xform=pynbody.analysis.angmom.calc_sideon_matrix,
+    cen_size="1 kpc",
+    disk_size="5 kpc",
+    cen=None,
+    vcen=None,
+    move_all=True,
+    **kwargs
+):
+    """
+
+    Reposition and rotate the simulation containing the halo h to see
+    h's disk edge on.
+
+    Given a simulation and a subview of that simulation (probably the
+    halo of interest), this routine centers the simulation and rotates
+    it so that the disk lies in the x-z plane. This gives a side-on
+    view for SPH images, for instance.
+
+    """
+    from pynbody import transformation, filt
+    from pynbody.analysis import halo
+
+    global config
+
+    if move_all:
+        top = h.ancestor
+    else:
+        top = h
+
+    # Top is the top-level view of the simulation, which will be
+    # transformed
+
+    if cen is None:
+        # or h['pos'][h['phi'].argmin()]
+        cen = halo.center(h, retcen=True, **kwargs)
+
+    tx = transformation.inverse_translate(top, cen)
+
+    if vcen is None:
+        vcen = halo.vel_center(h, retcen=True, cen_size=cen_size)
+
+    tx = transformation.inverse_v_translate(tx, vcen)
+
+    # Use stars from inner 10kpc to calculate angular momentum vector
+    if len(h.s) > 0:
+        cen = h.s[filt.Sphere(disk_size)]
+    else:
+        cen = h[filt.Sphere(disk_size)]
+
+    trans = vec_to_xform(pynbody.analysis.angmom.ang_mom_vec(cen))
+
+    tx = transformation.transform(tx, trans)
+
+    return tx
+
 #loading snapshot
 s=pynbody.load('/mnt/data0/jillian/gguaman/r239/cosmo25p.768sg1bwK1BHe75.007779')
 #s=pynbody.load('/mnt/data0/jillian/gguaman/r239/r239.007779.tipsy')
@@ -13,7 +70,8 @@ s.physical_units()
 
 #centering halo
 #pynbody.analysis.angmom.faceon(s)
-pynbody.analysis.angmom.sideon(s)
+#pynbody.analysis.angmom.sideon(s)
+stars_sideon(s)
 
 #creating star velocity plot
 sph.image(s.s,qty="vz",width='20 kpc',cmap="PuOr", log=False, units='km s**-1')
