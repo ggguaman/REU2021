370723aaaec5f2f5a86766ddb9ae53a39e41db6e
Giselle Guaman <gguaman@hamilton.internal.amnh.org>
2022-03-31 16:12:31 +0000

    gas and stellar velocity

--- r320/galaxy320_StelVelR.py
+++ r320/galaxy320_StelVelR.py
@@ -11,7 +11,7 @@ filename = '/mnt/data0/jillian/gguaman/r320/r320.007779.tipsy'
 redshift=0.01
 image_width=20
 
-stellar_map, stellar_pa, ax = plot_manga_map(filename, redshift, "star", weights="mass", image_width=image_width, orientation="sideon", cmap='PuOr', vmin=-70, vmax=70)
+stellar_map, stellar_pa, ax = plot_manga_map(filename, redshift, "star", weights="mass", image_width=image_width, orientation="sideon", cmap='PuOr', vmin=-80, vmax=80)
 
 #plt.show()
 plt.savefig("galaxy320_StelVelR.png")
