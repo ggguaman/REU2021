8c7ce2be1f793333fd41f8ac34564456edc2669d
Giselle Guaman <gguaman@hamilton.internal.amnh.org>
2022-04-07 17:55:49 +0000

    Ray stellar velocity

--- r142/galaxy142_StelVelR.py
+++ r142/galaxy142_StelVelR.py
@@ -11,7 +11,7 @@ filename='/mnt/data0/jillian/gguaman/r142/r142.007779.tipsy'
 redshift=0.01
 image_width=20
 
-stellar_map, stellar_pa, ax = plot_manga_map(filename, redshift, "star", weights="mass", image_width=image_width, orientation="sideon", cmap='PuOr', vmin=-100, vmax=100)
+stellar_map, stellar_pa, ax = plot_manga_map(filename, redshift, "star", weights="mass", image_width=image_width, orientation="sideon", cmap='PuOr', vmin=-150, vmax=150)
 
 #plt.show()
 plt.savefig("galaxy142_StelVelR.png")
